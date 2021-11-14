940af963ffdb9822735126717ffeb3d060559970
RikuTheKiller <vihervaara100@gmail.com>
2021-11-14 17:41:42 +0200

    Add project files.

---
+++ RikusExtensions/Extensions/CharacterStatModifiers.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using HarmonyLib;
+
+namespace RikusExtensions.Extensions
+{
+    [Serializable]
+    public class CharacterStatModifiersAdditionalData
+    {
+        public bool skipDiePatch;
+        public bool inDetermination;
+        public CharacterStatModifiersAdditionalData()
+        {
+            skipDiePatch = false;
+            inDetermination = false;
+        }
+    }
+    public static class CharacterStatModifiersExtension
+    {
+        public static readonly ConditionalWeakTable<CharacterStatModifiers, CharacterStatModifiersAdditionalData> data =
+            new ConditionalWeakTable<CharacterStatModifiers, CharacterStatModifiersAdditionalData>();
+
+        public static CharacterStatModifiersAdditionalData GetAdditionalData(this CharacterStatModifiers cs)
+        {
+            return data.GetOrCreateValue(cs);
+        }
+
+        public static void AddData(this CharacterStatModifiers cs, CharacterStatModifiersAdditionalData value)
+        {
+            try
+            {
+                data.Add(cs, value);
+            }
+            catch (Exception) { }
+        }
+
+    }
+    // Resets additional stat modifiers when "ResetStats" is called.
+    [HarmonyPatch(typeof(CharacterStatModifiers), "ResetStats")]
+    class CharacterStatModifiersPatchResetStats
+    {
+        private static void Prefix(CharacterStatModifiers __instance)
+        {
+            __instance.GetAdditionalData().skipDiePatch = false;
+            __instance.GetAdditionalData().inDetermination = false;
+        }
+    }
+}
---
+++ RikusExtensions/RikusExtensions.cs
@@ -0,0 +1,29 @@
+﻿using BepInEx;
+using UnboundLib;
+using UnboundLib.Cards;
+using HarmonyLib;
+
+namespace RikusExtensions
+{
+    [BepInDependency("com.willis.rounds.unbound", BepInDependency.DependencyFlags.HardDependency)]
+    [BepInDependency("pykess.rounds.plugins.moddingutils", BepInDependency.DependencyFlags.HardDependency)]
+    [BepInPlugin(ModId, ModName, Version)]
+    [BepInProcess("Rounds.exe")]
+    public class RikusExtensions : BaseUnityPlugin
+    {
+        private const string ModId = "com.RikuTheKiller.RikusExtensions";
+        private const string ModName = "RikusExtensions";
+        public const string Version = "1.0.0";
+        public static RikusExtensions instance { get; private set; }
+
+        void Awake()
+        {
+            var harmony = new Harmony(ModId);
+            harmony.PatchAll();
+        }
+        void Start()
+        {
+            instance = this;
+        }
+    }
+}
